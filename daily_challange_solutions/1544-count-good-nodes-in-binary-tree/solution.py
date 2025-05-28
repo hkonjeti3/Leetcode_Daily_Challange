143b2d9445e666518f53680b55a812f65cfa94d9
hkonjeti3 <149438712+hkonjeti3@users.noreply.github.com>
2025-05-28 22:47:38 +0000

    [temp-sync] Runtime - 136 ms (75.24%), Memory - 32.1 MB (82.44%)

---
+++ daily_challange_solutions/1544-count-good-nodes-in-binary-tree/solution.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        self.good=0
+        def dfs(node,cmax):
+            if not node:
+                return
+            
+            if node.val>=cmax:
+                self.good+=1
+                cmax=node.val
+
+            dfs(node.left,cmax)
+            dfs(node.right,cmax)
+            return 
+        
+        dfs(root,float('-inf'))
+        return self.good
